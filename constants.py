bdd23eaed71bf462ef006b60fb296925274c140d
Benjamin Maitland <marthinwurer@gmail.com>
2018-12-21 15:26:16 -0500

    Starting out

---
+++ constants.py
@@ -0,0 +1,6 @@
+
+import numpy as np
+import pint
+
+ureg = pint.UnitRegistry()
+
---
+++ oneD.py
@@ -0,0 +1,8 @@
+
+from constants import ureg
+
+# Goals: 1d version of primitive equations from hansen et al
+# with units
+# with finite volumes
+# start with forward euler
+
