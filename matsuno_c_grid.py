8e86713c54a7820a81c23026ab3ebaf89437d4a5
Benjamin Maitland <marthinwurer@gmail.com>
2020-01-24 21:45:43 -0500

    Got the C grid advection of velocity working!

--- matsuno_c_grid.py
+++ matsuno_c_grid.py
@@ -40,24 +40,76 @@ def ijm(q):
     return unit_roll(q, 1, 0)
 
 
+def imjp(q):
+    return imj(ijp(q))
+
+
 def advection_of_velocity_u(u, v, dx):
-    return NotImplemented
-    # ipj is i+1/2,j ; imj is i-1/2,j
-    u_ipj = (unit_roll(u, -1, 0) + u) / 2
-    u_imj = (unit_roll(u, 1, 0) + u) / 2
-    du_ipj = unit_roll(u, -1, 0) - u
-    du_imj = u - unit_roll(u, 1, 0)
-    v_ijp = (unit_roll(v, -1, 1) + v) / 2
-    v_ijm = (unit_roll(u, 1, 1) + v) / 2
-    du_ijp = unit_roll(u, -1, 1) - u
-    du_ijm = u - unit_roll(u, 1, 1)
+    """
+    grid is:
+    i h ip
+    P U P
+    V   V
+    P U P
+
+    so we need to get u at half and v at half
+    """
+    """
+    * = current U
+    vu = V * U, both need to be interpolated
+    uu = U * U, needs to be interpolated and squared
+    im   i    ip
+    U    U     U
+    P  V vu V  P  j
+    U uu *  uu U
+    P  V vu V  P  jp
+    U    U     U
+    """
+    # velocity terms
+    u_ipj = (ipj(u) + u) / 2
+    u_imj = (imj(u) + u) / 2
+    # these names are relative to U, not to p
+    v_ijm = (imj(v) + v) / 2
+    v_ijp = (imjp(v) + ijp(v)) / 2
+
+    # gradient terms
+    du_ipj = (ipj(u) - u)
+    du_imj = (u - imj(u))
+    du_ijp = (ijp(u) - u)
+    du_ijm = (u - ijm(u))
 
     finite = (u_ipj * du_ipj + u_imj * du_imj +
-              v_ijp * du_ijp + v_ijm * du_ijm) / (2 * dx)
+              v_ijp * du_ijp + v_ijm * du_ijm) / dx
     return finite
 
 
 def advection_of_velocity_v(u, v, dx):
+    """
+    """
+    """
+    im   i    ip
+    V    V    V
+    P  U vv U P j
+    V uv * uv V
+    P  U vv U P jp
+    V    V    V
+    """
+    # these names are relative to V, not to p
+    # velocity terms
+    v_ijp = (ijp(v) + v) / 2
+    v_ijm = (ijm(v) + v) / 2
+    u_ipj = (u + ijm(u)) / 2
+    u_imj = (imj(u) + imjp(u)) / 2
+
+    # gradient terms
+    dv_ipj = (ipj(v) - v)
+    dv_imj = (v - imj(v))
+    dv_ijp = (ijp(v) - v)
+    dv_ijm = (v - ijm(v))
+
+    finite = (u_ipj * dv_ipj + u_imj * dv_imj +
+              v_ijp * dv_ijp + v_ijm * dv_ijm) / dx
+    return finite
     return NotImplemented
     # ipj is i+1/2,j ; imj is i-1/2,j
     u_ipj = (unit_roll(u, -1, 0) + u) / 2
@@ -99,18 +151,18 @@ def advection_of_geopotential(u, v, p, dx):
 
 
 def matsumo_scheme(u, v, p, dx, dt):
-    u_star = u - dt * (#advection_of_velocity_u(u, v, dx) +
+    u_star = u - dt * (advection_of_velocity_u(u, v, dx) +
                        geopotential_gradient_u(p, dx))
     # v_star = v
-    v_star = v - dt * (#advection_of_velocity_v(u, v, dx) +
+    v_star = v - dt * (advection_of_velocity_v(u, v, dx) +
                        geopotential_gradient_v(p, dx))
     p_star = p - dt * advection_of_geopotential(u, v, p, dx)
 
     geo_u_star = geopotential_gradient_u(p_star, dx)
-    u_next = u - dt * (#advection_of_velocity_u(u_star, v_star, dx) +
+    u_next = u - dt * (advection_of_velocity_u(u_star, v_star, dx) +
                        geo_u_star)
     # v_next = v
-    v_next = v - dt * (#advection_of_velocity_v(u_star, v_star, dx) +
+    v_next = v - dt * (advection_of_velocity_v(u_star, v_star, dx) +
                        geopotential_gradient_v(p_star, dx))
     pit_star = advection_of_geopotential(u_star, v_star, p_star, dx)
     p_next = p - dt * pit_star
@@ -119,7 +171,7 @@ def matsumo_scheme(u, v, p, dx, dt):
 
 
 def main():
-    side_len = 8
+    side_len = 64
     dx = 300 * units.km
     dt = 900 * units.s
     half = side_len // 2
@@ -128,8 +180,9 @@ def main():
     H = 1000 * units.m
     p = np.zeros((side_len, side_len), dtype=np.float) * units.m
     p[:] = H
-    p[half: half + 2, half:half+2] += 1 * units.m
-    p[1, 2] += 1 * units.m
+    # p[half: half + 2, half:half+2] += 1 * units.m
+    # p[1, 2] += 1 * units.m
+    v[half, half] += 1 * units.m / units.s
 
     plt.ion()
     plt.figure()
@@ -140,7 +193,7 @@ def main():
     initial_variation = get_total_variation(p)
     print("Initial Variation: %s" % (initial_variation,))
 
-    for i in range(400):
+    for i in tqdm.tqdm(range(400)):
         # print("iteration %s" % i)
         plt.clf()
         plt.imshow(p)
@@ -150,7 +203,7 @@ def main():
 
         u, v, p = matsumo_scheme(u, v, p, dx, dt)
         current_variation = get_total_variation(p)
-        if current_variation.m > initial_variation.m + 0.00001:
+        if current_variation.m > initial_variation.m + 0.1:
             print("iteration %s" % i)
             print("Variation too high: %s" % (current_variation,))
             # return False
